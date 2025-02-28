1c20dee138869a0d62ec9ec1e2e97271d6e34b99
Blood-Pirouette <bloodpirouette@tutanota.com>
2025-02-27 20:27:43 -0500

    feed forward

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-#define N 2 // number of inputs/ pixels
-#define M 4 // number of layers excluding the input layer
+#define N 1 // number of inputs/ pixels
+#define M 2 // number of layers excluding the input layer
 
 // transpose a given matrix of dimensions RxC
 // Paremeters: flattened pointer to input matrix, R, C, flattened pointer to output matrix
@@ -19,26 +20,29 @@ void multiplyMatrix(double *, double *, int, int, int, double *);
 // Paremeters: pointer to matrix, R, C
 void printMatrix(double *, int, int);
 
-// given the matrix A and B in AX=B, apply the guassian algorithm with partial pivoting to A and B
-// the dimensions are A: RxR, B: Rx1
-// Paremeters: flattened pointer to A, R
-void gaussianAlgorithm(double *, double *, int);
+// print the structure of the neural network
+// the layers array has M pointers corresponding to the number of layers
+// each element is a memory address pointing to a double array corresponding to all parameters
+// of all the neurons in the layer. the no_neurons array is a parallel array containing the number
+// of neurons in each layer
+// Parameters: flattened pointer to pointer array layers, flattened pointer to no_neurons array
+void printLayerNeurons(double **layers, int *no_neurons);
 
-// given the matrix A and B in AX=B, apply partial pivoting to A and B
-// the dimensions are A: RxR, B: Rx1
-// Paremeters: flattened pointer to A, R
-void pivotingAlgorithm(double *, double *, int);
-
-// given the matrix equation AX=B, solve for x using backward substitution
-// the dimensions are A: RxR, X: Rx1, B: Rx1
-// Paremeters: flattened pointer to A, flattened pointer to B, R, flattened pointer to X
-void backwardSubstitutionAlgorithm(double *, double *, int, double *);
-
-// build a neural network model of M layers, with Ni neurons in each layer i
+// randomly build a neural network model of M layers, with Ni neurons in each layer i
 // Paremeters: flattened pointer to pointer array to store layers, pointer to no_neurons which will be
 // a parallel array to the pointer array for layers and each i position will dictate the number of neurons
 // in the corresponding layer
 void buildModel(double **, int *);
+
+// manually build a neural network model of M layers, with Ni neurons in each layer i
+// Paremeters: flattened pointer to pointer array to store layers, pointer to no_neurons which will be
+// a parallel array to the pointer array for layers and each i position will dictate the number of neurons
+// in the corresponding layer
+void testBuildModel(double **, int *);
+
+// perform the sigmoid activation function on the output matrix of a layer
+// Paremeters: flattened output matrix of a layer, size of layer output matrix, flattened output of function
+void sigmoidActivation(double *, int, double *);
 
 // general equations:
 // memory address of element ij of flattened matrix M with C columns and memory p
@@ -49,35 +53,34 @@ void buildModel(double **, int *);
 int main()
 {
     // parameters to modify
-    double X[N + 1][1] = {1, 2, 3};              // input array
-    int no_neurons[M][1] = {{2}, {3}, {2}, {4}}; // array containing the number of neurons in each layer
-    int u = 2;                                   // learning rate
-
+    double X[N + 1][1] = {1, 0.33};    // input array
+    int no_neurons[M][1] = {{1}, {1}}; // array containing the number of neurons in each layer
+    int u = 2;                         // learning rate
     // algorithm parameters
+
     double *layers[M][1];
     srand(time(NULL));
-    buildModel((double **)layers, (int *)no_neurons);
+    // buildModel((double **)layers, (int *)no_neurons);
+    testBuildModel((double **)layers, (int *)no_neurons);
 
-    for (int i = 0; i < M; i++)
-    {
-        printf("Layer %d:\n\n", i+1);
-        double *sub_vector;
-        for (int j = 0; j < no_neurons[i][0]; j++)
-        {
-            if (i == 0)
-            {
-                sub_vector = layers[i][0] + j * no_neurons[i][0];
-                printf("Neuron %d:\n", j+1);
-                printMatrix((double *)sub_vector, 1, no_neurons[i][0]);
-            }
-            else
-            {
-                sub_vector = layers[i][0] + j * no_neurons[i-1][0];
-                printf("Neuron %d:\n", j+1);
-                printMatrix((double *)sub_vector, 1, no_neurons[i-1][0]);
-            }
-        }
-    }
+    // printLayerNeurons((double **)layers, (int *)no_neurons);
+
+    // multiply X and L(h)
+    double Zh[1][1];
+    multiplyMatrix((double *)layers[0][0], (double *)X, 1, N + 1, 1, (double *)Zh);
+    printMatrix((double *)Zh, 1, 1);
+    double Yh[2][1];
+    sigmoidActivation((double *)Zh, 1, (double *)Yh);
+    Yh[0][0] = 1;
+    printMatrix((double *)Yh, 2, 1);
+
+    // multiply X and L(f)
+    double Zf[1][1];
+    multiplyMatrix((double *)layers[1][0], (double *)Yh, 1, 2, 1, (double *)Zf);
+    printMatrix((double *)Zf, 1, 1);
+    double Yf[2][1];
+    sigmoidActivation((double *)Zf, 1, (double *)Yf);
+    printMatrix((double *)Yf, 2, 1);
 
     return 0;
 }
@@ -106,6 +109,24 @@ void buildModel(double **layers, int *no_neurons)
             *layers_j = rand() % 20;
         }
     }
+}
+
+void testBuildModel(double **layers, int *no_neurons)
+{
+    double **layer_1 = layers;
+    double **layer_2 = layers + 1;
+    *layer_1 = (double *)malloc(2 * sizeof(double));
+    *layer_2 = (double *)malloc(2 * sizeof(double));
+
+    double *layer_1_parameter_1 = *layer_1;
+    double *layer_1_parameter_2 = *layer_1 + 1;
+    *layer_1_parameter_1 = 1.86;
+    *layer_1_parameter_2 = -3.3;
+
+    double *layer_2_parameter_1 = *layer_2;
+    double *layer_2_parameter_2 = *layer_2 + 1;
+    *layer_2_parameter_1 = -1.5;
+    *layer_2_parameter_2 = 4.4;
 }
 
 void transposeMatrix(double *matrix, int R, int C, double *matrix_transpose)
@@ -143,78 +164,42 @@ void printMatrix(double *matrix, int R, int C)
     {
         for (int j = 0; j < C; j++)
         {
-            printf("%7.2lf ", *(matrix + i * C + j));
+            printf("%7.4lf ", *(matrix + i * C + j));
         }
         printf("\n");
     }
     printf("\n");
 }
 
-void gaussianAlgorithm(double *A, double *B, int R)
+void sigmoidActivation(double *Z, int size_z, double *Y)
 {
-    for (int i = 0; i < R; i++)
+    for (int i = 1; i < size_z+1; i++)
     {
-        pivotingAlgorithm(A, B, R);
-        for (int j = i + 1; j < R; j++)
-        {
-            double c = (*(A + j * R + i)) / (*(A + i * R + i));
-            for (int k = 0; k < R; k++)
-            {
-                double *A_jk = A + j * R + k;
-                *A_jk = *A_jk - c * (*(A + i * R + k));
-            }
-            double *B_j0 = B + j;
-            *B_j0 = *B_j0 - c * (*(B + i));
-        }
+        double *Y_i = Y + i;
+        *Y_i = 1 / (1 + exp((*(Z + i-1))*-1));
     }
 }
 
-void backwardSubstitutionAlgorithm(double *A, double *B, int R, double *X)
+void printLayerNeurons(double **layers, int *no_neurons)
 {
-    int n = R - 1;
-    double *X_n = X + n;
-    *X_n = (*(B + n)) / (*(A + n * R + n));
-    for (int i = n - 1; i >= 0; i--)
+    for (int i = 0; i < M; i++)
     {
-        double sum = 0;
-        for (int j = i + 1; j < R; j++)
+        printf("Layer %d:\n\n", i + 1);
+        double *sub_vector;
+        for (int j = 0; j < (*(no_neurons + i)); j++)
         {
-            sum += (*(A + i * R + j)) * (*(X + j));
-        }
-        double *X_i = X + i;
-        *X_i = ((*(B + i)) - sum) / (*(A + i * R + i));
-    }
-}
-
-void pivotingAlgorithm(double *A, double *B, int R)
-{
-    for (int i = 0; i < R; i++)
-    {
-        double max = abs(*(A + i * R + i));
-        int max_j = i;
-        for (int j = i + 1; j < R; j++)
-        {
-            if (max < abs(*(A + j * R + i)))
+            if (i == 0)
             {
-                max = *(A + j * R + i);
-                max_j = j;
+                sub_vector = *(layers + i) + j * (N + 1);
+                printf("Neuron %d:\n", j + 1);
+                printMatrix((double *)sub_vector, 1, (N + 1));
             }
-        }
-        if (max_j > i)
-        {
-            for (int k = 0; k < R; k++)
+            else
             {
-                double temp = *(A + i * R + k);
-                double *A_ik = A + i * R + k;
-                *A_ik = *(A + max_j * R + k);
-                double *A_max_jk = A + max_j * R + k;
-                *A_max_jk = temp;
+                sub_vector = *(layers + i) + j * ((*(no_neurons + i - 1)) + 1);
+                printf("Neuron %d:\n", j + 1);
+                printMatrix((double *)sub_vector, 1, *(no_neurons + i - 1) + 1);
             }
-            double tempB = *(B + i);
-            double *B_i0 = B + i;
-            *B_i0 = *(B + max_j);
-            double *B_max_j0 = B + max_j;
-            *B_max_j0 = tempB;
         }
     }
 }
